1b8ba2937c48a15496d70e4e7497f0be9f8d0c57
chavp <my.parinya@gmail.com>
2012-11-08 17:46:22 +0700

    first Commit
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ HTML5GamesWeb/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTML5GamesWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Css3()
+        {
+            return View();
+        }
+
+        public ActionResult PingPong()
+        {
+            return View();
+        }
+
+        public ActionResult Css3Transition()
+        {
+            return View();
+        }
+
+        public ActionResult Css3Flip()
+        {
+            return View();
+        }
+
+        public ActionResult MatchGame()
+        {
+            return View();
+        }
+
+        public ActionResult Untangle()
+        {
+            return View();
+        }
+    }
+}
---
+++ HTML5GamesWeb/Controllers/WorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTML5GamesWeb.Controllers
+{
+    public class WorldController : Controller
+    {
+        //
+        // GET: /World/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
